bec0f25afe49a7004823f2cb115b0e7cc9552147
Bhawna Sharma <shbhawna@In-shbhawna.local>
2018-10-24 15:31:22 +0530

    Renamed reducer function of findIntersection

--- functionsLib.js
+++ functionsLib.js
@@ -162,7 +162,7 @@ const findUnion = function(firstArray, secondArray){
   return findUnique(joinedArray);
 }
 
-const insertIdenticalElement = function(accumulator, element) {
+const extractIdenticalElement = function(accumulator, element) {
   let { firstArray, identicalElements } = accumulator;
   if(firstArray.includes(element)){
     identicalElements.push(element);
@@ -171,7 +171,7 @@ const insertIdenticalElement = function(accumulator, element) {
 }
 
 const findIntersection = function(firstArray, secondArray){
-  return  secondArray.reduce(insertIdenticalElement, { firstArray : firstArray, identicalElements : [] }).identicalElements;
+  return  secondArray.reduce(extractIdenticalElement, { firstArray : firstArray, identicalElements : [] }).identicalElements;
 }
 
 const findDifference = function(firstArray, secondArray){
